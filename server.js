8c6cfb9f366e4bad44a3dd9ee159bbdd157b748a
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2020-06-07 11:50:33 +0100

    configure database connection

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const errorHandler = require('_middleware/error-handler');
+const mongoose = require("mongoose");
+
+var uri = "mongodb://localhost:27017/hackatton";
+mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true });
+const connection = mongoose.connection;
+connection.once("open", function() {
+  console.log("MongoDB database connection established successfully");
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
